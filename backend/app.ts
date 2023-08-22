0d57c20df496bc6c53482090a97f2c6d6d1753c9
amit-sawhney <amitsawhney.github@gmail.com>
2023-08-21 19:49:33 -0500

    Fix default api route

--- backend/app.ts
+++ backend/app.ts
@@ -1,25 +1,25 @@
-import createError from "http-errors";
-import HttpStatus from "http-status-codes";
-import cors from "cors";
-import express, { Request, Response } from "express";
-import helmet from "helmet";
+import createError from 'http-errors';
+import HttpStatus from 'http-status-codes';
+import cors from 'cors';
+import express, { Request, Response } from 'express';
+import helmet from 'helmet';
 
-import routes from "./src/routes";
-import env from "./config/env";
+import routes from './src/routes';
+import env from './config/env';
 
 const app = express();
 
 app.use(helmet());
-app.use(express.json({ limit: "2.1mb" }));
-app.use(express.urlencoded({ limit: "2.1mb", extended: false }));
+app.use(express.json({ limit: '2.1mb' }));
+app.use(express.urlencoded({ limit: '2.1mb', extended: false }));
 
-if (env.NODE_ENV !== "production") {
+if (env.NODE_ENV !== 'production') {
   app.use(cors({ origin: /localhost:\d{4}/, credentials: true }));
 }
 
 // Routes
-app.use("/api", routes);
-app.get("/", (_req: Request, res: Response) => res.json("API working!"));
+app.use('/api', routes);
+app.get('/', (_req: Request, res: Response) => res.json('API working!'));
 
 // Handle case when no route is matched
 app.use(function (_req: Request, _res: Response, next) {
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,10 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const router = Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.get('/', function (req, res, next) {
+  res.json('Default route');
 });
 
 export default router;
