e3424809a509e7ec1c064a6238c7748d20c434f4
amit-sawhney <amitsawhney.github@gmail.com>
2023-08-31 11:36:38 -0500

    Setup tailwind on frontend

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div className="flex h-full text-center items-center justify-center">
+      <h1 className="text-2xl">Sign in to your account</h1>
+    </div>
+  );
+};
+
+export default Login;
